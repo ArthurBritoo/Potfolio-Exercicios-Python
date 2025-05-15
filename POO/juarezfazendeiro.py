358e4d2fe25057c774ea7157366e5ee3a307a8ac
ArthurBritoo <arthurbr.lima@gmail.com>
2025-05-15 18:41:22 -0300

    Update juarezfazendeiro.py

--- POO/juarezfazendeiro.py
+++ POO/juarezfazendeiro.py
@@ -36,3 +36,28 @@ def main():
         print("A soma da area da sua fazenda é ", fazenda.calcarea_total())
         print("O perimetro da sua fazenda é ", fazenda.calcperimetro_externo())
 main()
+
+
+#2. Expandindo seus negócios, Juarez entrou no ramo de veterinária e precisa de um programa capaz de cadastrar animais em sua base de dados. Como você mostrou que é um programador eficaz, ele designou essa tarefa a você. a. Desenvolva, utilizando orientação a objetos, um programa que consiga cadastrar animais que possua os seguintes atributos: 
+#Raça, Cor predominante, Peso e Nome do dono. No final, mostre na tela do programa 5 animais escolhidos por você e cadastrados no sistema. 
+class Animais:
+    def __init__(self, raca, cor_predominante, peso, nome_dono):
+        self.raca = raca
+        self.cor_predominante = cor_predominante
+        self.peso = peso
+        self.nome_dono = nome_dono
+    def mostrar_informacoes(self):
+        return (f"\nRaça: {self.raca}  \nCor Predominante:        {self.cor_predominante} \nPeso: {self.peso}kg \nNome do Dono: {self.nome_dono}\n")
+def main():
+    nmrAnimais = int(input("quantos animais você quer cadastrar? "))
+    animais_list = []
+    for i in range(nmrAnimais):
+        x = input(f"qual a raça do {i+1}.o animal: ")
+        y = input(f"qual a cor predominante do {i+1}.o animal: ")
+        z = float(input(f"qual o peso do {i+1}.o animal: "))
+        f = input(f"qual o nome do dono do {i+1}.o animal: ")
+        animais = Animais(x,y,z,f)
+        animais_list.append(animais)
+    for animais in animais_list:
+        print(animais.mostrar_informacoes())
+main()
