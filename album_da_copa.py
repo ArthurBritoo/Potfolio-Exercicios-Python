f5c08ac4dfda5b20a10afb1392442038555589cc
ArthurBritoo <arthurbr.lima@gmail.com>
2025-05-15 17:58:53 -0300

    Update album_da_copa.py

--- album_da_copa.py
+++ album_da_copa.py
@@ -1,3 +1,9 @@
-N, M = map(int, input().split())
-figurinhas = set(map(int, input().split()))
-print(N - len(figurinhas))
+#Enunciado:
+# Dado o total de figurinhas e uma lista das figurinhas compradas, calcule quantas faltam para completar o álbum.
+#Entrada:
+ #Número total N, seguido por M figurinhas compradas.
+#Saída:
+ #Número de figurinhas que faltam.
+#Resolução correta:
+def figurinhasFaltando(N, compradas):
+    return N - len(set(compradas))
