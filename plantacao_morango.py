0f1e5b8bac6a7f107ed1e47eec0b21f9de1f6608
ArthurBritoo <arthurbr.lima@gmail.com>
2025-05-15 17:58:19 -0300

    Update plantacao_morango.py

--- plantacao_morango.py
+++ plantacao_morango.py
@@ -1,4 +1,11 @@
-M, N = map(int, input().split())
-plantacao = [list(map(int, input().split())) for _ in range(M)]
-max_area = max(sum(linha) for linha in plantacao)
-print(max_area)
+#Enunciado:
+ #Os administradores da Fazenda Fartura planejam criar uma nova plantação de morangos, no formato retangular. Eles têm vários locais possíveis para a nova plantação, com diferentes dimensões de comprimento e largura. Para os administradores, o melhor local é aquele que tem a maior área.
+#Entrada:
+ #Quatro números inteiros representando as dimensões dos dois terrenos.
+#Saída:
+# Área do melhor local.
+#Resolução correta:
+
+def checarMaiorArea(la, ca, lb, cb):
+    a, b = la * ca, lb * cb
+    return max(a, b)
