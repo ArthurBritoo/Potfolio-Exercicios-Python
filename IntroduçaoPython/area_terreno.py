5a8820899d3c6964a84e2e6dd9adac5cf675ca86
Arthur Brito <arthurbr.lima@gmail.com>
2025-05-15 23:39:48 -0300

    Create  area_terreno.py

---
+++ IntroduçaoPython/area_terreno.py
@@ -0,0 +1,5 @@
+# 4. Calcular a área de um terreno retangular (imobiliária XYZ)
+base = float(input("Digite a base do terreno (em metros): "))
+altura = float(input("Digite a altura do terreno (em metros): "))
+area = base * altura
+print(f"A área do terreno é: {area} m²")
