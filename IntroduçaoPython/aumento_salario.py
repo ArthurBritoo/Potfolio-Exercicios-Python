6beb8c1900d3c037232589ce3a09161ed48d4f6b
Arthur Brito <arthurbr.lima@gmail.com>
2025-05-15 23:40:13 -0300

    Create aumento_salario.py

---
+++ IntroduçaoPython/aumento_salario.py
@@ -0,0 +1,4 @@
+# 5. Ler o salário de um funcionário e aumentá-lo em 20%
+salario_atual = float(input("Digite o salário atual: R$ "))
+novo_salario = salario_atual * 1.20
+print(f"O novo salário com 20% de aumento é: R$ {novo_salario:.2f}")
