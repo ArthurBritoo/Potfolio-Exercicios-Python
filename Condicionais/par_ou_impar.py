209b0299559790c36a4c7aa4b1be9c2f5aab2daf
Arthur Brito <arthurbr.lima@gmail.com>
2025-05-15 23:28:26 -0300

    Create par_ou_impar.py

---
+++ Condicionais/par_ou_impar.py
@@ -0,0 +1,6 @@
+# 1. Ler um número inteiro e dizer se é par ou ímpar
+numero = int(input("Digite um número inteiro: "))
+if numero % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
