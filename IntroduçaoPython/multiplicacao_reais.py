ec68f2a40595881eb3bc8608263943cdcf24ba68
Arthur Brito <arthurbr.lima@gmail.com>
2025-05-15 23:39:22 -0300

    Create  multiplicacao_reais.py

---
+++ IntroduçaoPython/multiplicacao_reais.py
@@ -0,0 +1,5 @@
+# 2. Exibir a multiplicação de dois números reais informados pelo usuário
+numero1 = float(input("Digite o primeiro número real: "))
+numero2 = float(input("Digite o segundo número real: "))
+resultado = numero1 * numero2
+print(f"O resultado da multiplicação é: {resultado}")
