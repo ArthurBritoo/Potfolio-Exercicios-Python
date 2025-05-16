1bfd0ef3cfe19a3f18330eb7dd0e78efc86a5c79
Arthur Brito <arthurbr.lima@gmail.com>
2025-05-15 22:45:45 -0300

    Update and rename while to idadealturas.py

---
+++ Loops/idadealturas.py
@@ -0,0 +1,21 @@
+#Crie um programa que |é as idades e alturas de alguns alunos. A condi¢ao de
+#parada é a altura = 0. Em seguida, o programa deve informar quantos alunos com
+#mais de 13 anos possuem altura inferior a 1
+
+altura = 1000
+idade =
+contador
+while altura |
+
+altura = float(input(
+
+if altura == 0:
+break
+
+idade = int (input(
+
+if idade > 13 and altura < 1.5:
+contador += 1
+
+print xd % (contador))]
+
