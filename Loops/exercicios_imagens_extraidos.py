bc1cfe6420ce3aaa84bf5769c42d2e7db2ead547
Arthur Brito <arthurbr.lima@gmail.com>
2025-05-15 22:43:02 -0300

    Add files via upload

---
+++ Loops/exercicios_imagens_extraidos.py
@@ -0,0 +1,101 @@
+# QUESTÃO 1
+1. Crie um programa que |é as idades e alturas de alguns alunos. A condi¢ao de
+parada é a altura = 0. Em seguida, o programa deve informar quantos alunos com
+mais de 13 anos possuem altura inferior a 1
+
+altura = 1000
+idade =
+contador
+while altura |
+
+altura = float(input(
+
+if altura == 0:
+break
+
+idade = int (input(
+
+if idade > 13 and altura < 1.5:
+contador += 1
+
+print xd % (contador))]
+
+Escreva um programa que lé uma quantidade indeterminada de numeros inteiros e
+escreve todos os que forem impares positivos (use o ‘continue’). Considerar o valor
+99 como fim da entrada.
+
+while x<1e8:
+resposta
+ resposta
+x+=2
+elif resposta
+
+break
+
+# QUESTÃO 2
+Faga um programa que imprima o fatorial de um numero. O valor de entrada deve
+ser menor ou igual a 20.
+
+while
+numero = int(input("Coloque um ntimero até 20: "))
+if numero >
+print("Nimero maior que vinte. Digite novamente.")
+continue
+
+fatorial = 1
+for i in range(1, numero + 1):
+fatorial *= i
+print("O fatorial de %d ¢ %d" % (numero , fatorial))
+break
+
+# QUESTÃO 3
+4. Faga um programa que identifique os 15 primeiros numeros primos (utilizando a
+instrugao break).
+
+y.py > (alr
+contador
+
+numero = 2
+while contador < 1
+
+e_primo = True
+for i in range(2, numero):
+if numero % i
+
+e_primo = False
+break
+if e_primo:
+print (numero
+contador
+numero
+
+= = } a dois numeros — base e expoente — calcule e mostre o
+Pitiwne numer veveue wo Segundo numero. Nao utilize a fungdo de poténcia da
+linguagem.
+
+base
+
+expoente =
+
+resultado
+
+for i in range(expoente:
+resultado *= base
+
+print xd <d " % (base , expoente, resultado)
+
+# QUESTÃO 4
+Faga um programa que pega 5 valores positivos do usuario (usando while). Caso 0
+usuario digite algum numero negativo, o programa deve terminar imediatamente.
+Caso termine normalmente, informe que os dados foram inseridos com sucesso (use
+0 else).
+for i in range(1,
+numero (input("Digite o ntimero inteiro: " % (i)))
+if numero < @:
+
+print("O numero é negativo")
+
+break
+
+print(|"0s dados foram inseridos com sucesso")
+
